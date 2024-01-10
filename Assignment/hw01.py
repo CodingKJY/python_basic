1b881d8a66bd503fdcaa838a56750be31f065108
kaiyang <kaiyang@alum.ccu.edu.tw>
2024-01-10 16:03:38 +0800

    upload hw01

---
+++ Assignment/hw01.py
@@ -0,0 +1,8 @@
+width  = input("Input width: ")
+height = input("Input height: ")
+print("Area = ", width * height)
+
+a = input("Input a: ")
+b = input("Input b: ")
+c = input("Input c: ")
+print("Average = ", (a + b + c)/3)
