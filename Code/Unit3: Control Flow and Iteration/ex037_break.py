25ac715cb85fa7b88d9f69d585b936d3c9fda4f2
kaiyang <kaiyang@alum.ccu.edu.tw>
2024-01-25 14:45:44 +0800

    reindex unit3 code, upload ex035-038

---
+++ Code/Unit3: Control Flow and Iteration/ex037_break.py
@@ -0,0 +1,7 @@
+while True:
+    num = eval(input("Input a even number: "))
+    if num % 2 == 0: 
+        print('Is even, break')
+        break
+    else:
+        print('Not even, input again\n')
---
+++ Code/Unit3: Control Flow and Iteration/ex038_break02.py
@@ -0,0 +1,8 @@
+while True:
+    while True:
+        num = eval(input("Input a even number: "))
+        if num % 2 == 0: 
+            print('Is even, break')
+            break
+        else:
+            print('Not even, input again\n')
